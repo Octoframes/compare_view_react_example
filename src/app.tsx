038cdaaf668d95c63fa9e87658fb7413b3a69594
christopher-besch <mail@chris-besch.com>
2022-07-18 18:58:20 +0200

    react functioning

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import CompareView from "compare_view";
-import Test from "./test";
 
 const ReactApp: React.FC = () => {
     return (
         <div>
             <h1>A bit of React using compare_view</h1>
-            {/* <Test /> */}
             <CompareView image_urls={["./cat.png", "./cat_grey.png"]} config={{ create_controls: true }} />
-            {/* <CompareView /> */}
-            {/* <CompareView image_urls={["./cat.png", "./cat_grey.png"]} /> */}
+            <CompareView image_urls={["./cat.png", "./cat_grey.png"]} />
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,3 @@ const container = document.getElementById("react-root") as HTMLElement;
 const root = createRoot(container);
 root.render(<ReactApp />);
 
-
-// import React from 'react'
-// import { ReactFromModule } from 'compare_view'
-// console.log(React === ReactFromModule) // true :)
-// console.log(React);
-// console.log(ReactFromModule);
