4cbe1513314f9626a9cc15973690fcde4f16bcff
christopher-besch <mail@chris-besch.com>
2022-08-03 14:10:35 +0200

    testing dynamic images; added action trigger for compare_view repo

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import CompareView from "compare_view";
+import CompareView, { load_new_imgs, CompareViewData } from "compare_view";
 
 const ReactApp: React.FC = () => {
+    // when buton pressed change images
+    let button_ref = React.createRef<HTMLButtonElement>();
+
+    // run when compare_view instance has been loaded
+    function attach_btn(cvd: CompareViewData): void {
+        button_ref.current?.addEventListener("click", () => {
+            load_new_imgs(cvd, ["./cat.png", "./cat_green.png"]);
+        });
+    }
+
     return (
         <div>
             <h1>A bit of React using compare_view</h1>
             <CompareView image_urls={["./cat.png", "./cat_grey.png"]} config={{ create_controls: true }} />
-            <CompareView image_urls={["./cat.png", "./cat_grey.png"]} />
+            <CompareView image_urls={["./cat.png", "./cat_grey.png"]} callback={attach_btn} />
+            <button ref={button_ref}>Wanna see a green cat?</button>
         </div>
     );
 }
